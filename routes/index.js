311c66f4e43cf63a77904c06cbf635c29b39489e
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-01-22 10:42:31 +0700

    create model and test api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-
+import { userRouter } from "./user.js"
 
 export const initRoutes = (app) => {
-    // app.use('/api/user', userRouter)
+    app.use('/api/user', userRouter)
     // app.use('/api/auth', authRouter)
     // app.use('/api/listing', listingRouter)
-    app.listen(7000, () => {
-        console.log('Server is running')
-    })
+    
 }
