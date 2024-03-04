3633e542b64172be666fd84e99a8d375111471cd
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-03-04 09:36:29 +0700

    config bearer token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -8,8 +8,11 @@ export const verifyToken = (req, res, next) => {
     // }
 
     //header :{authorization: Bearer token}
-    if (req?.header?.authorization?.startsWith("Bearer")) {
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
         const token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            return next(appError.errHandlerCustom(401, 'Unauthorized'));
+        }
         jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
             if (err) {
                 return next(appError.errHandlerCustom(401, 'Unauthorized'));
