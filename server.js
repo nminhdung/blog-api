9adaba3ac158fea3a79499f7bc0d175466ba348a
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-03-05 08:19:51 +0700

    fix bug path when deployed

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { dbConnect } from "./config/dbConnect.js";
 import { initRoutes } from "./routes/index.js";
+import path from 'path'
+
 
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 app.use(cors({
-    origin: [process.env.URL_CLIENT,"https://blog-ui-lilac.vercel.app"],
+    origin: [process.env.URL_CLIENT, "https://blog-ui-lilac.vercel.app"],
     credentials: true,
     method: ["POST", "PUT", "GET", "DELETE"],
     exposedHeaders: ["set-cookie"],
@@ -21,3 +24,7 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 initRoutes(app);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
